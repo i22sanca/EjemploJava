44b24efc42f45684cee4fafedd81cef656c1a032
i22sanca <ascabanillas94@gmail.com>
2020-02-19 01:29:05 +0100

    Subida inicial

---
+++ ejemplo/src/main/java/es/proyecto/ejemplo/App.java
@@ -0,0 +1,17 @@
+package es.proyecto.ejemplo;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static Logger log = Logger.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+    	log.info( "Hello World!" );
+    }
+}
